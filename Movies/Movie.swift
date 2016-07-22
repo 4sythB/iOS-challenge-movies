6822e161291c4502c718536143f2c4ba290c9d20
Brad Forsyth <b4syth@gmail.com>
2016-07-22 09:36:33 -0600

    Implemented movie model

--- Movies/Movie.swift
+++ Movies/Movie.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+class Movie {
+    
+    private let kTitle = "title"
+    private let kPopularity = "popularity"
+    private let kOverview = "overview"
+    private let kPoster = "poster_path"
+    
+    var title: String
+    var popularity: String
+    var overview: String
+    var poster: String
+    
+    init?(dictionary: [String:AnyObject]) {
+        guard let title = dictionary[kTitle] as? String, popularity = dictionary[kPopularity] as? String, overview = dictionary[kOverview] as? String, poster = dictionary[kPoster] as? String else { return nil }
+        
+        self.title = title
+        self.popularity = popularity
+        self.overview = overview
+        
+        let imageBaseURL = NSURL(string: "http://image.tmdb.org/t/p/w500")
+        let imageURL = imageBaseURL?.URLByAppendingPathComponent(poster)
+        
+        self.poster = String(imageURL)
+        
+        ImageController.imageForURL(self.poster) { (image) in
+            // TODO: - figure out image url
+        }
+    }
+}
